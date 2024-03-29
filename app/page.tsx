5dd10ac2c8d427f902622e8c0d0292638d016d92
Jola <nitariod@gmail.com>
2024-03-29 15:08:30 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,208 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="home-container">
+      <div className="side-bar">
+        <div className="profile">
+        <img
+          src="/display_pic.jpeg"
+          alt="display pic"
+          className="display_pic"
         />
+        <h2>ALEX JOHNSON</h2>
+        <p>alex.johnson@gmail.com</p>
+        </div>
+        <ul>
+          <li><div><img src="/house.png"/></div>DASHBOARD</li>
+          <li className="active"><div><img src="/flight.png"/></div>FLIGHTS</li>
+          <li><div><img src="/wallet.png"/></div>WALLET</li>
+          <li><div><img src="/book.png"/></div>REPORTS</li>
+          <li><div><img src="/statistics.png"/></div>STATISTICS</li>
+          <li><div><img src="/settings.png"/></div>SETTINGS</li>
+        </ul>
+        <h3>ACTIVE USERS</h3>
+        <div className="images">
+          <div>
+            <span className="image-1"><img src="/1.jpeg"  /></span>
+            <span className="image-2"><img src="/2.jpeg" /></span>
+            <span className="image-3"><img src="/3.jpeg" /></span>
+            <span className="image-4"><img src="/4.jpeg" /></span>
+            <span className="image-5"><span></span></span>
+          </div>
+        </div>
+        <div><img src="/world-2.png"/></div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="main-content">
+        <div className="top">
+          <div className="rows">
+            <div>
+              <span><div><img src="/location.png"/></div>NEW YORK (JFK)</span>
+              <span><span className="transfer"><div><img src="/transfer.png"/></div></span></span>
+              <span><div><img src="/location.png"/></div>MUMBAI (BOM)</span>
+            </div>
+            <div><span><div><img src="/calender.png"/></div>29 JULY 2019</span></div>
+            <div><span><div><img src="/person.png"/></div>2 TRAVELLER</span></div>
+          </div>
+          <div className="rows">
+            <div>
+              <span className="activate"><span>ONE WAY</span></span>
+              <span><span>ROUND TRIP</span></span>
+              <span><span>MULTI CITY</span></span>
+            </div>
+            <div><span><div><img src="/chair.png"/></div>FIRST CLASS</span></div>
+            <div className="activate"><span>SEARCH</span></div>
+          </div>
+        </div>
+        <div className="middle">
+          <div>
+            <h3>RESULTS (25)</h3>
+            <button>FILTER</button>
+            <button style={{display: "flex", alignItems: "center"}}>TICKET OF CLASS<div><img src="/caret-down.png" style={{width: "40px"}}/></div></button>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="b-left">
+            <ul>
+              <li>
+                <div className="cut-effect-top"></div>
+                <div className="b-row">
+                  <div>
+                    <img src="/emirates.png"/>
+                  </div>
+                  <div>
+                    <span>JFK</span>
+                    <span>13:00</span>
+                  </div>
+                  <div>
+                    <span>EMIRATES</span>
+                    <span>11H 20M</span>
+                    <span>NON_STOP</span>
+                  </div>
+                  <div>
+                    <span>BOM</span>
+                    <span>14:20</span>
+                  </div>
+                  <div>
+                    <span>$1,572</span>
+                    <button>BOOK NOW</button>
+                  </div>
+                </div>
+                <div className="cut-effect-bottom"></div>
+              </li>
+              <li>
+                <div className="cut-effect-top"></div>
+                <div className="b-row">
+                  <div>
+                    <img src="/qatar.png"/>
+                  </div>
+                  <div>
+                    <span>JFK</span>
+                    <span>13:00</span>
+                  </div>
+                  <div>
+                    <span>QATAR AIRWAYS</span>
+                    <span>11H 20M</span>
+                    <span>NON_STOP</span>
+                  </div>
+                  <div>
+                    <span>BOM</span>
+                    <span>14:20</span>
+                  </div>
+                  <div>
+                    <span>$2,072</span>
+                    <button>BOOK NOW</button>
+                  </div>
+                </div>
+                <div className="cut-effect-bottom"></div>
+              </li>
+              <li>
+                <div className="cut-effect-top"></div>
+                <div className="b-row">
+                  <div>
+                    <img src="/lufthansa.png"/>
+                  </div>
+                  <div>
+                    <span>JFK</span>
+                    <span>13:00</span>
+                  </div>
+                  <div>
+                    <span>LUFTHANSA</span>
+                    <span>11H 20M</span>
+                    <span>NON_STOP</span>
+                  </div>
+                  <div>
+                    <span>BOM</span>
+                    <span>14:20</span>
+                  </div>
+                  <div>
+                    <span>$1,872</span>
+                    <button>BOOK NOW</button>
+                  </div>
+                </div>
+                <div className="cut-effect-bottom"></div>
+              </li>
+              <li>
+                <div className="cut-effect-top"></div>
+                <div className="b-row">
+                  <div>
+                    <img src="/emirates.png"/>
+                  </div>
+                  <div>
+                    <span>JFK</span>
+                    <span>13:00</span>
+                  </div>
+                  <div>
+                    <span>EMIRATES</span>
+                    <span>11H 20M</span>
+                    <span>NON_STOP</span>
+                  </div>
+                  <div>
+                    <span>BOM</span>
+                    <span>14:20</span>
+                  </div>
+                  <div>
+                    <span>$1,872</span>
+                    <button>BOOK NOW</button>
+                  </div>
+                </div>
+                <div className="cut-effect-bottom"></div>
+              </li>
+            </ul>
+          </div>
+          <div className="b-right">
+            <div>
+              <div>
+                <span>FROM</span>
+                <span>JFK</span>
+              </div>
+              <div>
+                <span>NON-STOP</span>
+              </div>
+              <div>
+                <span>TO</span>
+                <span>BOM</span>
+              </div>
+            </div>
+            <div>
+              <img src="/w.png"/>
+            </div>
+            <div className="base">
+              <div>
+                <button className="activate">NON STOP</button>
+                <button>ONE STOP</button>
+                <button>MORE STOP</button>
+              </div>
+              <div>
+                <h4>PRICE</h4>
+              </div>
+              <div>
+                <hr/>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
